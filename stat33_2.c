76f29a1c43f234b822d0269649673ca91d66ef8a
Ali <114328925+amAliMannan@users.noreply.github.com>
2024-08-06 02:47:22 +0300

    Update stat33_2.c

--- stat33_2.c
+++ stat33_2.c
@@ -7,6 +7,7 @@ int main() {
     srand(time(NULL));
     int success = 0;
     int all = 0;
+    int previousAttempt = 0;
 
     for (int i = 1; i <= 6; i++) {
         for (int j = 1; j <= 6; j++) {
